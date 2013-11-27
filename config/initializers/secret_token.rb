414a873bf84db81ce47d68d24fe0fab0c9daef34
GilmoO <gilmo90@gmail.com>
2013-11-27 12:05:57 +0400

    [generate] rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toggrep::Application.config.secret_key_base = '74c71f91b91ef754f09660963d787b55d29f78288995074201c86eaba5832eccac2ffe4a6b88f2717a3111eb9f844dc325a2737857172bfe0051247988c862de'
