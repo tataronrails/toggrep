b766ee0547083da6f8e0f0cda096d512b42afa18
IldarKamalov <ildar.kamalov15@gmail.com>
2013-12-20 15:37:13 +0400

    [add] gravatar for user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def gravatar_for(user)
+    size = 90
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm"
+    image_tag(gravatar_url, class: "circular ui image")
+  end
 end
