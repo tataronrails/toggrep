e6ac6b8c69d2b2fdc7e0c711b34cbc0663727b82
Alpesh Khambhala <alpesh.ror@gmail.com>
2014-10-21 15:28:59 +0530

    fixed small issues

--- app/controllers/toggl_projects_controller.rb
+++ app/controllers/toggl_projects_controller.rb
@@ -1,8 +1,8 @@
 class TogglProjectsController < ApplicationController
 
   def project_users
-    api_key = current_user.toggl_api_key
-    toggl_project_users = TogglProject.project_users(api_key, params[:project_id])
+    api_key = current_user.toggl_api_key    
+    toggl_project_users = TogglProject.project_users(current_user, params[:project_id])
     users = toggl_project_users.reduce({}) do |memo, pu|
       user = User.find_by_toggl_id(pu.uid)
       memo[user.id] = user.toggl_user.fullname if user
@@ -18,7 +18,7 @@ class TogglProjectsController < ApplicationController
     projects = toggl_projects.map{ |p| p.slice(:name, :id, :wid).to_hash }
     api_key = current_user.toggl_api_key
     response = projects.reduce({}) do |memo, p|
-      w_name = TogglWorkspace.find_name_by_id(api_key, p['wid'])
+      w_name = TogglWorkspace.find_name_by_id(current_user, p['wid'])
       memo[w_name] ||= []
       memo[w_name] << p.slice('id', 'name')
       memo
