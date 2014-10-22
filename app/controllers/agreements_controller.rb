e5abc60f0cd8e68c6c97566156c12a2fbeb6e091
Alpesh Khambhala <alpesh.ror@gmail.com>
2014-10-22 13:20:46 +0530

    fixed issues based on review comment

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -27,7 +27,7 @@ class AgreementsController < ResourcesController
   end
 
   def show
-    @time_entries = Agreement.get_time_entries(@agreement.worker, @agreement.project_id)
+    @time_entries = Agreement.get_time_entries(@agreement.worker, @agreement.project_id, 4.weeks)
   end
   
   private
