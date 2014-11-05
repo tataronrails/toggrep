24316a3d4a3aeb428a8c987e4e8d0888e7a3e071
Vlad Alive <vladalive@gmail.com>
2014-11-05 20:59:22 +0300

    [config] development: do not send emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,4 +23,6 @@ Toggrep::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.perform_deliveries = false
 end
