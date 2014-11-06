6d3369a9aa20f4a9be5cea2513ef0ad4a147951f
Ivan Ananev <ivcheg@gmail.com>
2014-11-07 01:17:24 +0300

    [fix] wrong shared files

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,8 +41,8 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
-    run "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
-    run "ln -nfs #{shared_path}/config/settings.yml #{release_path}/config/settings.yml"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/email.yml #{release_path}/config/email.yml"
   end
   after 'deploy:finalize_update', 'deploy:symlink_config'
 
